85c6ff7a31a4a27900f749502d6cf427bb1f9d36
JooYoung1121 <rlawndud7@nate.com>
2018-06-29 00:43:15 +0900

    다시

---
+++ app/src/main/java/com/example/jooyoung/freeder/ListItem.java
@@ -0,0 +1,9 @@
+package com.example.jooyoung.freeder;
+
+import android.graphics.drawable.Drawable;
+
+public class ListItem {
+    private Drawable img;
+    private String Title;
+
+}
--- app/src/main/java/com/example/jooyoung/freeder/MainActivity.java
+++ app/src/main/java/com/example/jooyoung/freeder/MainActivity.java
@@ -6,14 +6,18 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Intent _intent;
     ListView d_day;
-    Button dday,calender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
 
 
